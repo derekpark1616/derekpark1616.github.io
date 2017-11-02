44e286b93fe9ffbc490f3b574beea34add403544
Derek Park <derekpark1616@gmail.com>
2017-11-02 13:43:34 -0700

    Clean up/react install
    Installed react and modularized js from html file

---
+++ index.js
@@ -0,0 +1,41 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import './index.css';
+
+
+function resizeIFrame(iFrame) {
+	iFrame.width = iFrame.contentWindow.document.body.scrollWidth;
+	iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+}
+
+window.addEventListener('DOMContentLoaded', function(e) {
+	var iFrame = document.getElementById('pageFrame');
+	resizeIFrame(iFrame);
+});
+			
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1} 
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+				
+		for (i = 0; i < dots.length; i++) {
+			dots[i].className = dots[i].className.replace(" active", "");
+		}
+		slides[slideIndex-1].style.display = "block"; 
+		dots[slideIndex-1].className += " active";
+	}
+}
